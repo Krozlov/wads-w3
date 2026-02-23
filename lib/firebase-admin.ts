551c16a6d1c6fcf400bf78b5ac113dfa8e89f626
Krozlov <lkencana06@gmail.com>
2026-02-23 17:30:10 +0700

    final push

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,11 +1,18 @@
 import * as admin from "firebase-admin";
 
+function getPrivateKey(): string {
+  const key = process.env.FIREBASE_PRIVATE_KEY ?? "";
+  return key
+    .replace(/^"|"$/g, "")
+    .replace(/\\n/g, "\n");
+}
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      privateKey: getPrivateKey(),
     }),
   });
 }
